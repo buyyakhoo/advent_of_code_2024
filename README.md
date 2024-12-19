e9ab592fbf043ddb72583581bf91ceabeadfc34b
buyyakhoo <pollapat13799@gmail.com>
2024-12-19 20:48:00 +0700

    day18_part2

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Copying code without thoughtful consideration leads to a deficiency in problem-s
 ## **Purposed**
 - To better understand the solution when you encounter challenges.
 - To enhance proficiency in algorithmic thinking, abstraction, and computer science concepts, including data structures and beyond.
+
+## **Noted**
+- day_18 part 2: suggest to change from recursion to dynamic programming to optimize the algorithm because Big-O, O(4^n), is too high which leads to find the answer very slow. I will do the dynamic programming version as soon as possible.
---
+++ day_18/ram_run_part2.js
@@ -0,0 +1,79 @@
+import fs from 'fs';
+
+const file = fs.readFileSync("./input.txt", "utf8")
+
+const data_all = file.replaceAll("\r", "")
+                    .split("\n")
+                    .map(d => d.split(",")
+                                .map(n => parseInt(n))
+                        )
+                    // .filter((f, idx) => idx < 12) // 1024
+                    
+const data_old = data_all.filter((f, idx) => idx < 1024)
+const data_new = data_all.filter((f, idx) => idx >= 1024)
+const data = data_old.slice();
+
+const count_list = []
+const save_cor_list = new Map()
+
+const x_t_use = data_all.map(n => n[0]).sort((a, b) => b-a)[0]
+const y_t_use = data_all.map(n => n[1]).sort((a, b) => b-a)[0]
+
+const find_shortest_path = (x=0, y=0, x_target, y_target, count=0) => {
+    
+    if (x < 0 || y < 0 || x > x_target-1 || y > y_target-1) {
+        return
+    }
+
+    if (x === x_target-1 && y === y_target-1) {
+        count_list.push(count)
+        return
+    }
+
+    const cor = `${x},${y}`
+    if (save_cor_list.get(cor) && save_cor_list.get(cor) <= count) {
+        return
+    }
+
+    save_cor_list.set(cor, count)
+
+    if (data.find(data_cor => data_cor[0] === x && data_cor[1] === y)) {
+        return
+    }
+
+    // this find the counting
+
+    find_shortest_path(x+1, y, x_target, y_target, count+1)
+    find_shortest_path(x, y+1, x_target, y_target, count+1)
+    find_shortest_path(x-1, y, x_target, y_target, count+1)
+    find_shortest_path(x, y-1, x_target, y_target, count+1)   
+}
+
+data_new.forEach((dn) => {
+    data.push([dn[0], dn[1]])
+    find_shortest_path(0, 0, x_t_use+1, y_t_use+1, 0)
+
+    if (count_list.length === 0) {
+        console.log(`result: ${dn[0]}, ${dn[1]}`)
+        return;
+    }
+
+    console.log(`${dn[0]}, ${dn[1]} passed!`)
+    console.log(count_list)
+    count_list.splice(0, count_list.length)
+    save_cor_list.clear()
+})
+
+
+
+
+
+// find_shortest_path(0, 0, x_t_use+1, y_t_use+1, 0)
+// console.log(`result: ${count_list.sort((a, b) => a-b)[0]}`)
+
+// fs.writeFileSync('./output.txt', 
+//     JSON.stringify(Array.from(save_cor_list)),
+//     () => {
+//         console.log('successfully saved')
+//     }
+// )
